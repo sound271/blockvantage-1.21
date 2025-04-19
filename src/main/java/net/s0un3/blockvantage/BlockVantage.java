3959a12859e4341280d8f7846e0372b0acfbcf3b
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2025-04-19 16:55:49 +0200

    Fluff Trim

--- src/main/java/net/s0un3/blockvantage/BlockVantage.java
+++ src/main/java/net/s0un3/blockvantage/BlockVantage.java
@@ -2,12 +2,9 @@ package net.s0un3.blockvantage;
 
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class BlockVantage implements ModInitializer {
-	public static final String MOD_ID = "blockvantage";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+//	public static final String MOD_ID = "blockvantage";
+//	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
