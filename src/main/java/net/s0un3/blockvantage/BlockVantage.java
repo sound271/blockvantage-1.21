98115f3e9f7f43effb3427c810e64ad5d02a1d03
sound271 <125661789+sound271@users.noreply.github.com>
2025-04-19 13:40:20 +0300

    Initial commit

---
+++ src/main/java/net/s0un3/blockvantage/BlockVantage.java
@@ -0,0 +1,16 @@
+package net.s0un3.blockvantage;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BlockVantage implements ModInitializer {
+	public static final String MOD_ID = "blockvantage";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/s0un3/blockvantage/BlockVantageClient.java
@@ -0,0 +1,9 @@
+package net.s0un3.blockvantage;
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockVantageClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("It works!!");
+    }
+}
