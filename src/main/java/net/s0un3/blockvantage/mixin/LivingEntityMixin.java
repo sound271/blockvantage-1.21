599d1e238c10318c465c8eec16615c1bdb69b44e
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2025-05-05 17:55:27 +0200

    wowie, this shield bash is amazing

--- src/main/java/net/s0un3/blockvantage/mixin/LivingEntityMixin.java
+++ src/main/java/net/s0un3/blockvantage/mixin/LivingEntityMixin.java
@@ -1,29 +1,45 @@
 package net.s0un3.blockvantage.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ShieldItem;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    @Inject(method = "damage", at = @At("HEAD"))
-    private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        LivingEntity entity = (LivingEntity)(Object)this;
-        if (!(entity instanceof PlayerEntity player)) return;
-        if (!player.isBlocking()) return;
-        if (player.getActiveItem().getItem() instanceof ShieldItem) {
-            onShieldBlocked(player);
-        }
+    @Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damageShield(F)V"))
+    private void onDamage(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (!((LivingEntity)(Object)this instanceof PlayerEntity player)) return;
+        onShieldBlocked(player);
+    }
+
+    @Inject(method = "pushAwayFrom", at = @At("HEAD"))
+    private void onPush(Entity entity, CallbackInfo ci) {
+        LivingEntity entityPushed = (LivingEntity)(Object)this;
+        if(entityPushed instanceof PlayerEntity) return;
+        if(!(entity instanceof PlayerEntity player)) return;
+        if (player.getStatusEffects().stream().noneMatch(effect -> effect.getEffectType() == StatusEffects.STRENGTH && effect.getAmplifier() == 4)) return;
+        if(entityPushed.getWorld().isClient) return;
+        DamageSource damageSource = new DamageSource(
+                entityPushed.getWorld().getRegistryManager()
+                        .getOrThrow(RegistryKeys.DAMAGE_TYPE)
+                        .getEntry(DamageTypes.GENERIC.getValue()).get()
+        );
+        entityPushed.damage((ServerWorld) entityPushed.getWorld(), damageSource, 5.0F);
+        entityPushed.setVelocity(player.getMovementDirection().getDoubleVector().multiply(3).add(0, 1, 0));
     }
 
     @Unique
